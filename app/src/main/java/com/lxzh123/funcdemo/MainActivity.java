3dd886163f2853960af85a31fb058daf8e00c63f
ljf1239848066 <1239848066@qq.com>
2018-07-21 10:10:37 +0800

    change MainActivity to ListActivity

--- app/src/main/java/com/lxzh123/funcdemo/MainActivity.java
+++ app/src/main/java/com/lxzh123/funcdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.lxzh123.funcdemo;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.lxzh123.bcreceiver.BCReceiverActivity;
 import com.lxzh123.bootstart.BootStartActivity;
@@ -20,60 +22,75 @@ import com.lxzh123.sendemail.SendMailActivity;
 import com.lxzh123.sortalgo.SortActivity;
 import com.lxzh123.weather.WeatherActivity;
 
-public class MainActivity extends Activity{
+public class MainActivity extends ListActivity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
-	//方法修饰符必须为public，private和protected都不行
-	public void OnButtonCLick(View v) {
-		Intent intent = new Intent();
-		switch (v.getId()) {
-		case R.id.btnQuickMark:
-			intent.setClass(MainActivity.this, QuickMarkActivity.class);
-			break;
-		case R.id.btnSendMail:
-			intent.setClass(MainActivity.this, SendMailActivity.class);
-			break;
-		case R.id.btnSaveDataByXml:
-			intent.setClass(MainActivity.this, XmlSaveDataActivity.class);
-			break;
-		case R.id.btnNotification:
-			intent.setClass(MainActivity.this, NotificationActivity.class);
-			break;
-		case R.id.btnFileBrowser:
-			intent.setClass(MainActivity.this, FileBrowserActivity.class);
-			break;
-		case R.id.btnRecord:
-			intent.setClass(MainActivity.this, RecordActivity.class);
-			break;
-		case R.id.btnRecord1:
-			intent.setClass(MainActivity.this, SoundRecorderActivity.class);
-			break;
-		case R.id.btnInputMethod:
-			intent.setClass(MainActivity.this, InputMethodActivity.class);
-			break;
-		case R.id.btnLocation:
-			intent.setClass(MainActivity.this, LocationActivity.class);
-			break;
-		case R.id.btnWeather:
-			intent.setClass(MainActivity.this, WeatherActivity.class);
-			break;	
-		case R.id.btnFtp:
-			intent.setClass(MainActivity.this, FTPActivity.class);
-			break;
-		case R.id.btnBCReceiver:
-			intent.setClass(MainActivity.this, BCReceiverActivity.class);
-			break;
-		case R.id.btnBootStart:
-			intent.setClass(MainActivity.this, BootStartActivity.class);
-			break;
-        case R.id.btnSort:
-            intent.setClass(MainActivity.this, SortActivity.class);
-            break;
+		String[] funcStr=new String[]{
+		        "QuickMark",
+                "SendMail",
+                "SaveDataByXml",
+                "Notification",
+                "FileBrowser",
+                "Record",
+                "Record1",
+                "InputMethod",
+                "Location",
+                "Weather",
+                "FTP",
+                "BCReceiver",
+                "BootStart",
+                "Sort Algo"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.main,funcStr);
+        this.setListAdapter(adapter);
+        this.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                switch (position) {
+                    case 0:
+                        intent.setClass(MainActivity.this, QuickMarkActivity.class);
+                        break;
+                    case 1:
+                        intent.setClass(MainActivity.this, SendMailActivity.class);
+                        break;
+                    case 2:
+                        intent.setClass(MainActivity.this, XmlSaveDataActivity.class);
+                        break;
+                    case 3:
+                        intent.setClass(MainActivity.this, NotificationActivity.class);
+                        break;
+                    case 4:
+                        intent.setClass(MainActivity.this, FileBrowserActivity.class);
+                        break;
+                    case 5:
+                        intent.setClass(MainActivity.this, RecordActivity.class);
+                        break;
+                    case 6:
+                        intent.setClass(MainActivity.this, SoundRecorderActivity.class);
+                        break;
+                    case 7:
+                        intent.setClass(MainActivity.this, InputMethodActivity.class);
+                        break;
+                    case 8:
+                        intent.setClass(MainActivity.this, LocationActivity.class);
+                        break;
+                    case 9:
+                        intent.setClass(MainActivity.this, WeatherActivity.class);
+                        break;
+                    case 10:
+                        intent.setClass(MainActivity.this, FTPActivity.class);
+                        break;
+                    case 11:
+                        intent.setClass(MainActivity.this, BCReceiverActivity.class);
+                        break;
+                    case 12:
+                        intent.setClass(MainActivity.this, BootStartActivity.class);
+                        break;
+                    case 13:
+                        intent.setClass(MainActivity.this, SortActivity.class);
+                        break;
 //		case R.id.btnCycleWheelView:
 //			intent.setClass(MainActivity.this, CycleWheelViewActivity.class);
 //			break;
@@ -89,10 +106,11 @@ public class MainActivity extends Activity{
 //		case R.id.btnViewPager:
 //			intent.setClass(MainActivity.this, ViewPagerActivity.class);
 //			break;
-		default:
-			return;
-		}
-		startActivity(intent);
+                    default:
+                        return;
+                }
+                startActivity(intent);
+            }
+        });
 	}
-	
 }
--- app/src/main/java/com/lxzh123/weather/WeatherActivity.java
+++ app/src/main/java/com/lxzh123/weather/WeatherActivity.java
@@ -1,15 +1,23 @@
 package com.lxzh123.weather;
 
-import com.lxzh123.funcdemo.R;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
+
+import com.lxzh123.funcdemo.R;
 
 public class WeatherActivity extends Activity {
 
 	private TextView weatherInfo;
+	private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            weatherInfo.setText(new GetWeather("武汉市","0").toString());
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +27,8 @@ public class WeatherActivity extends Activity {
 
 			@Override
 			public void run() {
-				weatherInfo.setText(new GetWeather("Îäºº","0").toString());
+			    handler.sendEmptyMessage(0);
+
 				super.run();
 			}
         	
