a5a838554f50051167b3aaa04f4854fb2dd5fe37
ljf1239848066 <1239848066@qq.com>
2018-08-06 20:53:13 +0800

    socket bugfix

--- app/src/main/java/com/lxzh123/funcdemo/socket/SocketClientActivity1.java
+++ app/src/main/java/com/lxzh123/funcdemo/socket/SocketClientActivity1.java
@@ -18,6 +18,9 @@ import com.lxzh123.funcdemo.R;
 import com.lxzh123.funcdemo.xml.XmlSaveDataActivity;
 
 import org.apache.log4j.helpers.OptionConverter;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.net.InetSocketAddress;
 
@@ -95,126 +98,149 @@ public class SocketClientActivity1 extends Activity {
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
     public void OnButtonClicked(View view){
         log.d(TAG, "OnButtonClicked");
         switch (view.getId()) {
             case R.id.btConnect:
                 log.d(TAG, "OnButtonClicked:R.id.btConnect");
-                final String ip=etIP.getText().toString();
-                final String port=etPort.getText().toString();
-                new Thread(){
-                    @Override
-                    public void run() {
-                        super.run();
-                        Message message=Message.obtain();
-                        if(ip.equals("")||port.equals("")){
-                            message.what=1;
-                            message.obj="请输入服务器Ip和端口号";
-                            handler.sendMessage(message);
-                            return;
-                        }
-                        try {
-                            client=new SocketClient();
-//                            client.Connect(new InetSocketAddress("localhost",SocketServerService.PORT));
-                            client.Connect(new InetSocketAddress(ip, OptionConverter.toInt(port,SocketServerService.PORT)));
-
-                            SharedPreferences preferences=getSharedPreferences("XmlSaveData.xml", XmlSaveDataActivity.MODE_PRIVATE);
-                            SharedPreferences.Editor editor=preferences.edit();
-                            editor.putString("savedip",ip);
-                            editor.putString("savedport",port);
-                            editor.commit();//存数据
-
-                            message.what=0;
-                            if(client.getState()){
-                                message.obj="Connect Succeed\n";
-                            }else{
-                                message.obj="Connect Failed\n";
-                            }
-                            handler.sendMessage(message);
-                        } catch (Exception ex) {
-                            System.out.println(ex.toString());
-                        }
-                    }
-                }.start();
-
+//                doConnect();
+                EventBus.getDefault().post("");
                 break;
             case R.id.btDisConnect:
                 log.d(TAG, "OnButtonClicked:R.id.btDisConnect");
-                if(client!=null){
-                    client.Close();
-                    tvReceiveMsg.append("DisConnected\n");
-                }
+                doDisConnect();
                 break;
             case R.id.btSend:
                 log.d(TAG, "OnButtonClicked:R.id.btSend");
-                final String msg=etSend.getText().toString().trim();
-                if(msg.equals("")){
-                    Toast.makeText(getBaseContext(), "请输入要发送的消息!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(client==null||!client.getState()){
-                    Toast.makeText(getBaseContext(), "请先连接服务器!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                new Thread(){
-                    @Override
-                    public void run() {
-                        try {
-                            String receive=client.SendAndReceive(msg, 30000);
-
-                            Message message=Message.obtain();
-                            message.what=0;
-                            message.obj=receive;
-                            handler.sendMessage(message);
-                        } catch (Exception ex) {
-                            System.out.println(ex.toString());
-                        }
-
-                        super.run();
-                    }
-                }.start();
+                doSendMsg();
                 break;
-
             default:
                 log.d(TAG, "OnButtonClicked:default");
                 break;
         }
     }
 
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    public void onMessageEvent(Object obj) {
+        final String ip=etIP.getText().toString();
+        final String port=etPort.getText().toString();
+        Message message=Message.obtain();
+        if(ip.equals("")||port.equals("")){
+            message.what=1;
+            message.obj="请输入服务器Ip和端口号";
+        }else{
+            try {
+                client=new SocketClient();
+//                            client.Connect(new InetSocketAddress("localhost",SocketServerService.PORT));
+                client.Connect(new InetSocketAddress(ip, OptionConverter.toInt(port,SocketServerService.PORT)));
 
-//
-//	private void receiveMsg() {
-//		try {
-//			// 客户端socket指定服务器的地址和端口号
-//			socket = new Socket("192.168.43.239", 8888);
-////			socket = new Socket("192.168.1.113", 8080);
-//			System.out.println("Socket=" + socket);
-//			// 同服务器原理一样
-//			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//			pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-//			for (int i = 0; i < 10; i++) {
-//				pw.println("howdy " + i);
-//				pw.flush();
-//				String str = br.readLine();
-//				tvText.append(str);
-//				System.out.println(str);
-//			}
-//			pw.println("END");
-//			pw.flush();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				System.out.println("close......");
-//				br.close();
-//				pw.close();
-//				socket.close();
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+                SharedPreferences preferences=getSharedPreferences("XmlSaveData.xml", XmlSaveDataActivity.MODE_PRIVATE);
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.putString("savedip",ip);
+                editor.putString("savedport",port);
+                editor.commit();//save data to xml
+
+                message.what=0;
+                if(client.getState()){
+                    message.obj="Connect Succeed\n";
+                }else{
+                    message.obj="Connect Failed\n";
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.toString());
+            }
+        }
+        handler.sendMessage(message);
+    }
+
+    private void doConnect(){
+        final String ip=etIP.getText().toString();
+        final String port=etPort.getText().toString();
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                Message message=Message.obtain();
+                if(ip.equals("")||port.equals("")){
+                    message.what=1;
+                    message.obj="请输入服务器Ip和端口号";
+                    handler.sendMessage(message);
+                    return;
+                }
+                try {
+                    client=new SocketClient();
+//                            client.Connect(new InetSocketAddress("localhost",SocketServerService.PORT));
+                    client.Connect(new InetSocketAddress(ip, OptionConverter.toInt(port,SocketServerService.PORT)));
+
+                    SharedPreferences preferences=getSharedPreferences("XmlSaveData.xml", XmlSaveDataActivity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putString("savedip",ip);
+                    editor.putString("savedport",port);
+                    editor.commit();//save data to xml
+
+                    message.what=0;
+                    if(client.getState()){
+                        message.obj="Connect Succeed\n";
+                    }else{
+                        message.obj="Connect Failed\n";
+                    }
+                    handler.sendMessage(message);
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+        }.start();
+    }
+
+    private void doDisConnect(){
+        if(client!=null){
+            client.Close();
+            tvReceiveMsg.append("DisConnected\n");
+        }
+    }
+
+    private void doSendMsg(){
+        final String msg=etSend.getText().toString().trim();
+        if(msg.equals("")){
+            Toast.makeText(getBaseContext(), "请输入要发送的消息!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(client==null||!client.getState()){
+            Toast.makeText(getBaseContext(), "请先连接服务器!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    String receive=client.SendAndReceive(msg, 30000);
+
+                    Message message=Message.obtain();
+                    message.what=0;
+                    message.obj=receive;
+                    handler.sendMessage(message);
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                }
+
+                super.run();
+            }
+        }.start();
+    }
 
     @Override
     protected void onDestroy() {
--- app/src/main/java/com/lxzh123/funcdemo/socket/SocketServerService.java
+++ app/src/main/java/com/lxzh123/funcdemo/socket/SocketServerService.java
@@ -13,7 +13,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class SocketServerService extends IntentService {
 
@@ -54,57 +53,73 @@ public class SocketServerService extends IntentService {
         startListen();
     }
 
-    private void startListen(){
-        Socket socket = null;
-        BufferedReader br = null;
-        PrintWriter pw = null;
-        Scanner scanner=new Scanner(System.in);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
         try {
-            // 设定服务端的端口号
-            serverSocket = new ServerSocket(PORT);
-            System.out.println("ServerSocket Start:" + serverSocket);
-            // 等待请求,此方法会一直阻塞,直到获得请求才往下走
-            socket = serverSocket.accept();
-            System.out.println("Connection accept socket:" + socket);
-            // 用于接收客户端发来的请求
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            // 用于发送返回信息,可以不需要装饰这么多io流使用缓冲流时发送数据要注意调用.flush()方法
-            pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-            System.out.println("start to read data from client:" + socket);
-            int bt=0;
-            char buffer[]=new char[1024];
-            String str;
-            while((bt=br.read(buffer))!=-1){
-                str=new String(buffer,0,bt);
-                System.out.println("Client Socket Message:" + str);
-                pw.println("Message Received:"+str);
-                pw.flush();
-            }
-//            while (true) {
-//                String str = br.readLine();
-////                tvText.append("Client Socket Message:" + str);
-//                System.out.println("Client Socket Message:" + str);
-////                Thread.sleep(1000);
-////                tvText.append("Message Received");
-//                str=scanner.next();
-//                pw.println("Message Received");
-//                pw.flush();
-//                if(str.equals("exit")){
-//                    break;
-//                }
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("Close.....");
-            try {
-                br.close();
-                pw.close();
-                socket.close();
-                serverSocket.close();
-            } catch (Exception e2) {
+            serverSocket.close();
+        } catch (Exception e2) {
 
+        }
+    }
+
+    private void startListen(){
+        if(serverSocket==null){
+            try {
+                // 设定服务端的端口号
+                serverSocket = new ServerSocket(PORT);
+                System.out.println("ServerSocket Start:" + serverSocket);
+
+                Socket socket;
+                // 等待请求,此方法会一直阻塞,直到获得请求才往下走
+                while((socket = serverSocket.accept())!=null){
+                    // 有连接接入后创建消息处理线程后台接收消息，继续等待其他客户端接入
+                    readDataThread(socket);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Close.....");
             }
         }
+    }
+
+    private void readDataThread(final Socket socket){
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                System.out.println("Connection accept socket:" + socket);
+                BufferedReader br = null;
+                PrintWriter pw = null;
+                // 用于接收客户端发来的请求
+                try {
+                    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    // 用于发送返回信息,可以不需要装饰这么多io流使用缓冲流时发送数据要注意调用.flush()方法
+                    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                    System.out.println("start to read data from client:" + socket);
+                    int bt=0;
+                    char buffer[]=new char[1024];
+                    String str;
+                    while((bt=br.read(buffer))!=-1){
+                        str=new String(buffer,0,bt);
+                        System.out.println("Client Socket Message:" + str);
+                        pw.println("Message Received:"+str);
+                        pw.flush();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Close.....");
+                    try {
+                        socket.close();
+                        br.close();
+                        pw.close();
+                    } catch (Exception e2) {
+
+                    }
+                }
+            }
+        }.start();
     }
 }
