97990cfc86a719e966780369a09ae95aec0151d5
ljf1239848066 <1239848066@qq.com>
2018-08-10 00:04:26 +0800

    change unsafe to independent module

--- unsafe/src/main/java/sun/reflect/Reflection.java
+++ unsafe/src/main/java/sun/reflect/Reflection.java
@@ -25,7 +25,11 @@
 
 package sun.reflect;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
