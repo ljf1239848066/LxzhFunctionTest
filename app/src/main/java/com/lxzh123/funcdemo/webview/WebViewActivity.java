f862dbb467599bccaf73be7c1733ad3d04a021ef
leijf <leijinfeng@geetest.com>
2019-09-12 12:02:53 +0800

    add webview test

--- app/src/main/java/com/lxzh123/funcdemo/webview/WebViewActivity.java
+++ app/src/main/java/com/lxzh123/funcdemo/webview/WebViewActivity.java
@@ -2,13 +2,24 @@ package com.lxzh123.funcdemo.webview;
 
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 
 import com.lxzh123.funcdemo.R;
 
 public class WebViewActivity extends Activity {
+
+    private static final String TAG = "WebViewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,7 @@ public class WebViewActivity extends Activity {
         setContentView(R.layout.layout_activity_web_view);
 
         RelativeLayout view = findViewById(R.id.layout_webview_container);
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)view.getLayoutParams();
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
         layoutParams.width = RelativeLayout.LayoutParams.MATCH_PARENT;
         layoutParams.height = 300;
 
@@ -24,5 +35,56 @@ public class WebViewActivity extends Activity {
         webView.setLayoutParams(layoutParams);
         webView.loadUrl("file:////android_asset/webview_test.html");
         view.addView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                Log.d(TAG, "WebViewClient:shouldOverrideUrlLoading:uri=" + uri.toString());
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Log.d(TAG, "WebViewClient:onPageStarted:url=" + url);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.d(TAG, "WebViewClient:onPageFinished:url=" + url);
+                super.onPageFinished(view, url);
+            }
+        };
+        WebChromeClient webChromeClient = new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Log.d(TAG, "WebChromeClient:onJsAlert:message=" + message);
+                return super.onJsAlert(view, url, message, result);
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                Log.d(TAG, "WebChromeClient:onProgressChanged:newProgress=" + newProgress);
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                return super.onJsConfirm(view, url, message, result);
+            }
+
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
+
+        };
+        webView.setWebViewClient(webViewClient);
+        webView.setWebChromeClient(webChromeClient);
     }
 }
